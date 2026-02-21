68d6ea88acbdceb95f31ebc71edb4f475fbc1fb5
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-21 14:55:43 +0530

    Add files via upload

---
+++ while loop 8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,streak,temp=0,count=0;
+    scanf("%d",&n);
+    while(n>=0){
+        scanf("%d",&streak);
+        if(streak==0){
+            count++;
+            if(count>temp){
+            temp=count;
+        }
+        }else{
+            count=0;
+        }
+    n--;
+    }
+    printf("Longest Default Streak:%d",temp);
+}

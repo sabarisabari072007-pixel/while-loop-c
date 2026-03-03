ed1a7331ed00c7f5a44492fa83236fd7e012f282
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-03-03 14:11:45 +0530

    Add files via upload

---
+++ arr1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a, m1 = -1, m2 = -1;
+    scanf("%d", &a);
+    int arr[a];
+    for(int i = 0; i < a; i++) {
+        scanf("%d", &arr[i]);   // FIXED
+        if(arr[i] > m1) {
+            m1 = arr[i];
+        }
+    }
+    for(int i = 0; i < a; i++) {
+        if(arr[i] > m2 && arr[i] != m1) {
+            m2 = arr[i];
+        }
+    }
+    printf("second:%d", m2);
+    return 0;
+}

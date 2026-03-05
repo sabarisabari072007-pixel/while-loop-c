cf13bade691d08ba21c8947d3ec9649902f18b16
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-03-05 10:29:36 +0530

    Add files via upload

---
+++ arr16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int min=arr[0];
+    int max=arr[1]-arr[0];
+    for(i=1;i<n;i++){
+        int profit=arr[i]-min;
+        if(profit>max){
+            max=profit;
+        }
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("%d",max);
+    
+    return 0;
+}
